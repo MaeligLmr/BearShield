67584eb1662d06952a215f3e6f1e66e8c127cacf
Dassonville Morgane <morganedassonville08@gmail.com>
2025-09-18 11:36:33 +0200

    [FEAT] - Add cart logic and styling cart page

---
+++ assets/js/cart-manager.js
@@ -0,0 +1,56 @@
+class CartManager {
+    constructor() {
+        this.storageKey = 'bearshield_cart';
+        this.cart = this.loadCart();
+    }
+
+    loadCart() {
+        const stored = localStorage.getItem(this.storageKey);
+        return stored ? JSON.parse(stored) : [];
+    }
+
+    saveCart() {
+        localStorage.setItem(this.storageKey, JSON.stringify(this.cart));
+    }
+
+    addProduct(product, customization = {}) {
+        const cartItem = {
+            id: Date.now(),
+            productId: product.id,
+            name: product.name,
+            image: product.image,
+            price: product.price,
+            customization: customization,
+            quantity: 1,
+            addedAt: new Date().toISOString()
+        };
+
+        this.cart.push(cartItem);
+        this.saveCart();
+        return cartItem;
+    }
+
+    removeProduct(cartItemId) {
+        this.cart = this.cart.filter(item => item.id !== cartItemId);
+        this.saveCart();
+    }
+
+    getCart() {
+        return this.cart;
+    }
+
+    getTotal() {
+        return this.cart.reduce((total, item) => total + (item.price * item.quantity), 0);
+    }
+
+    clearCart() {
+        this.cart = [];
+        this.saveCart();
+    }
+
+    getItemCount() {
+        return this.cart.reduce((count, item) => count + item.quantity, 0);
+    }
+}
+
+window.cartManager = new CartManager();
